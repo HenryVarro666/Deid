f09ae87d3baadc6841fc9dcdcfce3c66eba3ce52
HenryVarro666 <1504517223@qq.com>
2023-10-26 17:27:18 -0500

    Understand Testing

--- Implicit_understand.py
+++ Implicit_understand.py
@@ -1,11 +1,7 @@
-
-
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import transformers
 import torch
 import os
-
-
 from bs4 import BeautifulSoup
 import time
 from tqdm import tqdm
@@ -14,21 +10,17 @@ from tqdm import tqdm
 # input_directory = './testing-PHI-Gold-fixed'
 # model = "tiiuae/falcon-7b"
 # model_name_part = model.split("/")[-1]
-# output_path = "./rewrite_{}_explicit2".format(model_name_part)
+# output_path = "./rewrite_{}_implicit2".format(model_name_part)
 
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-# input_directory = './testing-PHI-Gold-fixed'
 input_directory = './Understand_Test'
-
+# model = "LLaMA1_7B"
 model = "Llama-2-7b-chat-hf"
-# model = "LLaMA2_7B"
 model_name_part = model
-output_path = "./rewrite_{}_explicit_understand".format(model_name_part)
+output_path = "./rewrite_{}_implicit_understand".format(model_name_part)
 
-# Explicit_prompt= "Please anonymize the following clinical note. Specifically, replace all the following information with the term “[redacted]”: redact any strings that might be a name or acronym or initial, redact any strings separated by the \/ symbol, redact patients' names, doctors' names and the strings in front of M.D. or after Dr., redact pager names and medical staff names, redact any strings that look like something years old or age 37, redact any dates and IDs and numbers and record dates, redact locations and addresses and clinic names, redact professions and ages and contacts, redact any acronyms and initials.: \n"
-Explicit_prompt= ''
+Implicit_prompt = "Please anonymize the following text: \n"
+
+
 
 rewrite_path = output_path
 directory = input_directory
@@ -36,13 +28,19 @@ directory = input_directory
 if not os.path.exists(rewrite_path):
     os.makedirs(rewrite_path)
 
+
+
 tokenizer = AutoTokenizer.from_pretrained(model)
-model = AutoModelForCausalLM.from_pretrained(model,trust_remote_code=True).to(device)
+model = AutoModelForCausalLM.from_pretrained(model,trust_remote_code=True).to("cuda")
+
+
+
 
 list_of_text_contents = []
 list_of_files = []
 
-# iterate over files in that directory
+# iterate over files in
+# that directory
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     # checking if it is a file
@@ -56,11 +54,10 @@ for filename in os.listdir(directory):
             list_of_text_contents.append(text_content)
 
 for i in tqdm(range(len(list_of_text_contents)), desc="Processing files"):
-    prompt = Explicit_prompt + list_of_text_contents[i]
-    inputs = tokenizer.encode(prompt, return_tensors='pt', truncation=True, max_length=2048).to(device)
+    prompt = Implicit_prompt + list_of_text_contents[i]  
+    inputs = tokenizer.encode(prompt, return_tensors='pt', truncation=True, max_length=2048)
 
-
-        # Run the model
+    # Run the model
     output = model.generate(inputs)
     rewrite_finding = tokenizer.decode(output[0], skip_special_tokens=True)
     rewrite_file = os.path.join(rewrite_path, list_of_files[i] + "_anonymized.txt")
@@ -68,6 +65,9 @@ for i in tqdm(range(len(list_of_text_contents)), desc="Processing files"):
     with open(rewrite_file, "w") as f:
         f.write(rewrite_finding)
 
-    print("-----------Anonymized " + "\n-----------")
-    print(output)
+    # print("-----------第" + str(i + 1) + "个\n-----------")
+    # # print("-----------My prompt " + "\n-----------")
+    # # print(prompt)
+    # print("-----------Anonymized " + "\n-----------")
+    # print(rewrite_finding)
 
